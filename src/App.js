65c83b95f47bb0db0372befc7e742721a24bbc7a
mohamedismail99 <mirsirajudeen@tekion.com>
2024-04-25 21:51:31 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
-import React, { useState, useEffect, useReducer } from 'react'
+import React from 'react'
 import './App.css';
-import EmployeeTable from './Components/EmployeeTable';
-import EmployeeHeader from './Components/EmployeeHeader';
-import { reducer } from './Components/EmployeeTable/EmployeeTable.helper';
-import { INITIAL_STATE } from './Components/EmployeeTable/EmployeeTable.constants';
+import DashboardView from './Components/DashboardView';
+import ProductListView from './Components/ProductListView';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
-  const [open, setOpen] = useState(false);
-  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
-  const showDrawer = () => {
-    setOpen(true);
-  };
-  // const getData = () => {
-  //   var requestOptions = {
-  //     method: "GET",
-  //   };
-
-  //   fetch("http://localhost:3030/posts", requestOptions)
-  //     .then((response) => response.json())
-  //     .then((result) => dispatch({ type:'ON_SUBMIT', value: result }))
-  //     .catch((error) => console.log("error", error));
-  // };
 
   // useEffect(() => {
-  //   getData();
+  //   fetch('https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory').then(response => response.json())
+  //   .then(res => dispatch(setProductList(res) ));
   // }, []);
   return (
-    <div className='appContainer'>
-      <div className='employeeTableContainer'>
-        <EmployeeHeader state={state} showDrawer={showDrawer}/>
-        <EmployeeTable state={state} dispatch={dispatch} open={open} setOpen={setOpen}/>
+    <Provider store={store}>
+      <div className='appContainer'>
+        <DashboardView />
+        <ProductListView />
       </div>
-    </div>
+    </Provider>
   )
 }
 
